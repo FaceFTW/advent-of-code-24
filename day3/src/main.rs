2a502421af081fc96eb617c6c807c952e5cc46a8
Alex Westerman <alex@faceftw.dev>
2024-12-03 16:41:36 -0500

    Day 3 Part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,7 +4,11 @@
 use regex::Regex;
 use std::{fs::File, io::Read, u64};
 
-struct MultInstr(u64, u64);
+enum Instr {
+    MultInstr(u64, u64),
+    DoInstr,
+    DontInstr,
+}
 
 fn main() {
     let mut string = String::new();
@@ -18,7 +22,7 @@ fn main() {
     // let data = parse_data(string.as_str());
 
     part1(string.trim_end());
-    part2();
+    part2(&string.trim_end());
 }
 
 // fn parse_data(string: &str) -> Vec<> {
@@ -33,10 +37,10 @@ fn main() {
 
 fn part1(memory: &str) {
     let re = Regex::new(r"mul\(([0-9]+)\,([0-9]+)\)").expect("regex 🅱️roke, sorry :(");
-    let nums: Vec<MultInstr> = re
+    let nums: Vec<(u64, u64)> = re
         .captures_iter(memory)
         .map(|data| {
-            MultInstr(
+            (
                 data.get(1).unwrap().as_str().parse::<u64>().unwrap(),
                 data.get(2).unwrap().as_str().parse::<u64>().unwrap(),
             )
@@ -50,6 +54,40 @@ fn part1(memory: &str) {
     println!("Day 3 Part 1 result: {final_result}");
 }
 
-fn part2() {
-    // println!("Day 3 Part 2 result: {final_result}");
+fn part2(memory: &str) {
+    let re =
+        Regex::new(r"mul\(([0-9]+),([0-9]+)\)|do\(\)|don\'t\(\)").expect("regex 🅱️roke, sorry :(");
+    let nums: Vec<Instr> = re
+        .captures_iter(memory)
+        .map(|data| {
+            //check the first chars before the (
+            let instr_prefix = data.get(0).unwrap().as_str().split_once("(").unwrap().0;
+            match instr_prefix {
+                "do" => Instr::DoInstr,
+                "don't" => Instr::DontInstr,
+                "mul" => Instr::MultInstr(
+                    data.get(1).unwrap().as_str().parse::<u64>().unwrap(),
+                    data.get(2).unwrap().as_str().parse::<u64>().unwrap(),
+                ),
+                _ => panic!("regex is 🅱️ad :("),
+            }
+        })
+        .collect();
+
+    let mut enable_mul = true;
+    let final_result = nums.into_iter().fold(0u64, |acc, instr| match instr {
+        Instr::MultInstr(a, b) => match enable_mul {
+            true => acc + (a * b),
+            false => acc,
+        },
+        Instr::DoInstr => {
+            enable_mul = true;
+            acc
+        }
+        Instr::DontInstr => {
+            enable_mul = false;
+            acc
+        }
+    });
+    println!("Day 3 Part 2 result: {final_result}");
 }
