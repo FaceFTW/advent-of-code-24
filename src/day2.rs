9c3b6fad771b21663a806a9e951807b52bd422a5
Alex Westerman <alex@faceftw.dev>
2024-12-03 16:00:54 -0500

    Day 2 Part 2 (yucky)

--- src/day2.rs
+++ src/day2.rs
@@ -15,6 +15,7 @@ fn main() {
     let reports = parse_reports(string.as_str());
 
     day1(reports.as_slice());
+    day2(reports.as_slice());
 }
 
 fn parse_reports(string: &str) -> Vec<Report> {
@@ -72,4 +73,71 @@ fn day1(reports: &[Report]) {
     }
 
     println!("Results for Day 2 Part 1 using day2.txt: {safe_count}");
+}
+
+//Most of the Same, except the last fold operation is _slightly_ different
+fn day2(reports: &[Report]) {
+    let mut safe_count = 0;
+    for report in reports {
+        //Assertions
+        //Reports have at least two levels
+
+        //Take the first two levels
+        let (first, second) = (report[0], report[1]);
+        //establish trend
+        let trend = match first > second {
+            true => Trend::Decreasing,
+            false => Trend::Increasing,
+        };
+
+        //
+        match report.windows(2).into_iter().fold(true, |acc, pair| {
+            let (first, second) = (pair[0], pair[1]);
+            let pair_trend = match first > second {
+                true => Trend::Decreasing,
+                false => Trend::Increasing,
+            };
+
+            let diff = u64::abs_diff(first, second);
+
+            acc && (pair_trend == trend)
+                && (diff >= UNSAFE_MIN_THRESH)
+                && (diff <= UNSAFE_MAX_THRESH)
+        }) {
+            true => safe_count = safe_count + 1,
+            false => {
+                //Brute force but remove levels one at a time
+                match (0..report.len()).fold(false, |acc, test| {
+                    let mut temp_report = report.clone();
+                    temp_report.remove(test);
+
+                    //Take the first two levels again
+                    let (first, second) = (temp_report[0], temp_report[1]);
+                    //establish trend (shadowed)
+                    let trend = match first > second {
+                        true => Trend::Decreasing,
+                        false => Trend::Increasing,
+                    };
+                    acc || temp_report.windows(2).into_iter().fold(true, |acc, pair| {
+                        let (first, second) = (pair[0], pair[1]);
+                        let pair_trend = match first > second {
+                            true => Trend::Decreasing,
+                            false => Trend::Increasing,
+                        };
+
+                        let diff = u64::abs_diff(first, second);
+
+                        acc && (pair_trend == trend)
+                            && (diff >= UNSAFE_MIN_THRESH)
+                            && (diff <= UNSAFE_MAX_THRESH)
+                    })
+                }) {
+                    true => safe_count = safe_count + 1,
+                    false => (),
+                }
+            }
+        };
+    }
+
+    println!("Results for Day 2 Part 2 using day2.txt: {safe_count}");
 }
