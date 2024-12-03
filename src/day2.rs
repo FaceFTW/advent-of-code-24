9dc0cbd1ec0360f3baa8e989f7931f97e08292c5
Alex Westerman <alex@faceftw.dev>
2024-12-03 15:23:50 -0500

    Day 2 Part 1

---
+++ src/day2.rs
@@ -0,0 +1,75 @@
+use std::{fs::File, io::Read};
+
+//Useful Alias
+type Report = Vec<u64>;
+
+fn main() {
+    let mut string = String::new();
+    match File::open("day2.txt") {
+        Ok(mut file) => {
+            let _ = file.read_to_string(&mut string);
+        }
+        Err(e) => panic!("{e}"),
+    }
+
+    let reports = parse_reports(string.as_str());
+
+    day1(reports.as_slice());
+}
+
+fn parse_reports(string: &str) -> Vec<Report> {
+    string
+        .trim_end()
+        .split("\n")
+        .map(|raw| {
+            raw.split(" ")
+                .map(|e| e.parse::<u64>().expect("Report had some bad data!"))
+                .collect()
+        })
+        .collect()
+}
+
+#[derive(PartialEq)]
+enum Trend {
+    Increasing = 0,
+    Decreasing = 1,
+}
+
+const UNSAFE_MAX_THRESH: u64 = 3;
+const UNSAFE_MIN_THRESH: u64 = 1; //More flexible than eq check
+
+fn day1(reports: &[Report]) {
+    let mut safe_count = 0;
+    for report in reports {
+        //Assertions
+        //Reports have at least two levels
+
+        //Take the first two levels
+        let (first, second) = (report[0], report[1]);
+        //establish trend
+        let trend = match first > second {
+            true => Trend::Decreasing,
+            false => Trend::Increasing,
+        };
+
+        //
+        match report.windows(2).into_iter().fold(true, |acc, pair| {
+            let (first, second) = (pair[0], pair[1]);
+            let pair_trend = match first > second {
+                true => Trend::Decreasing,
+                false => Trend::Increasing,
+            };
+
+            let diff = u64::abs_diff(first, second);
+
+            acc && (pair_trend == trend)
+                && (diff >= UNSAFE_MIN_THRESH)
+                && (diff <= UNSAFE_MAX_THRESH)
+        }) {
+            true => safe_count = safe_count + 1,
+            false => (),
+        };
+    }
+
+    println!("Results for Day 2 Part 1 using day2.txt: {safe_count}");
+}
