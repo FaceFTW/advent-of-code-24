58d50c5be72f94236f4aee522eb01f680c5e573b
Alex Westerman <alex@faceftw.dev>
2024-12-03 16:03:10 -0500

    Create a common template

---
+++ src/template.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let mut string = String::new();
+    match File::open("day.txt") {
+        Ok(mut file) => {
+            let _ = file.read_to_string(&mut string);
+        }
+        Err(e) => panic!("{e}"),
+    }
+
+    let data = parse_data(string.as_str());
+
+    part1();
+    part2();
+}
+
+fn parse_data(string: &str) -> Vec<> {
+    string
+        .trim_end()
+        .split("\n")
+        .map(|raw| {
+
+        })
+        .collect()
+}
+
+fn part1(){
+
+}
+
+fn part2(){
+
+}
