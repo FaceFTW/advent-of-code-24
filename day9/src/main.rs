f3ee89bc22fd50056a0686197ef408ba3d2b9e94
Alex Westerman <alex@faceftw.dev>
2024-12-12 16:05:16 -0500

    Day 9 Part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::Read};
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Copy, Debug)]
 struct FileLayout {
     id: Option<u32>,
     len: usize,
@@ -18,7 +18,7 @@ fn main() {
     let data = parse_data(string.as_str());
 
     part1(data.as_slice());
-    part2();
+    part2(data.as_slice());
 }
 
 fn parse_data(string: &str) -> Vec<FileLayout> {
@@ -81,6 +81,69 @@ fn part1(data: &[FileLayout]) {
     println!("Day 9 Part 1 result: {final_result}");
 }
 
-fn part2() {
-    // println!("Day  Part 2 result: {final_result}");
+fn part2(data: &[FileLayout]) {
+    //create the actual blockmap
+    let mut blocks = Vec::from(data);
+
+    let mut back_idx = blocks.len() - 1;
+    while back_idx > 0 {
+        if let Some(_) = blocks[back_idx].id {
+            let mut insert_idx = 0;
+            loop {
+                if let None = blocks[insert_idx].id {
+                    if blocks[insert_idx].len > blocks[back_idx].len {
+                        let free_size = blocks[insert_idx].len;
+                        let file_size = blocks[back_idx].len;
+                        blocks[insert_idx] = blocks[back_idx];
+                        blocks[back_idx] = FileLayout {
+                            id: None,
+                            len: file_size,
+                        };
+                        blocks.insert(
+                            insert_idx + 1,
+                            FileLayout {
+                                id: None,
+                                len: free_size - file_size,
+                            },
+                        );
+                        break;
+                    }
+
+                    if blocks[insert_idx].len == blocks[back_idx].len {
+                        blocks[insert_idx] = blocks[back_idx];
+                        blocks[back_idx] = FileLayout {
+                            id: None,
+                            len: blocks[insert_idx].len,
+                        };
+                        break;
+                    }
+                }
+
+                if insert_idx == back_idx {
+                    break;
+                }
+
+                insert_idx += 1;
+            }
+        }
+        back_idx -= 1;
+    }
+
+    //create the actual blockmap for checksumming, too lazy to just use the existing strucutres
+    let mut blocks_expanded = Vec::new();
+    for file_layout in blocks {
+        if file_layout.len > 0 {
+            blocks_expanded.append(&mut vec![file_layout.id; file_layout.len as usize]);
+        }
+    }
+
+    let final_result =
+        blocks_expanded
+            .into_iter()
+            .enumerate()
+            .fold(0u64, |acc, (idx, id)| match id {
+                Some(id) => acc + (idx as u64 * id as u64),
+                None => acc,
+            });
+    println!("Day 9 Part 2 result: {final_result}");
 }
