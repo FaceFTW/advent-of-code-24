8c611b00b6610c804abdf10aee8555336dbaf675
Alex Westerman <alex@faceftw.dev>
2024-12-13 17:12:16 -0500

    Day 10 Part 2 (This was my original solution to part 1)

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     let data = parse_data(string.as_str());
 
     part1(&data);
-    part2();
+    part2(&data);
 }
 
 fn parse_data(string: &str) -> Vec<Vec<u32>> {
@@ -75,12 +75,61 @@ fn calc_trailheads(
                     count = count + calc_trailheads(data, (row, col + 1), visited);
                 }
             }
-            dbg!(count);
             count
         }
     }
 }
 
-fn part2() {
+fn part2(data: &Vec<Vec<u32>>) {
+    let mut final_result = 0;
+    for row in 0..data.len() {
+        for col in 0..data[0].len() {
+            if data[row][col] == 9 {
+                final_result = final_result + calc_graded_trailheads(data, (row, col));
+            }
+        }
+    }
+
+    println!("Day 10 Part 2 result: {final_result}");
     // println!("Day 10 Part 2 result: {final_result}");
+}
+
+fn calc_graded_trailheads(
+    data: &Vec<Vec<u32>>,
+    pos: (usize, usize),
+    // visited: &mut Vec<(usize, usize)>,
+) -> u64 {
+    let (row, col) = pos;
+    match data[row][col] {
+        // 0 if visited.contains(&pos) => 0,
+        0 => 1,
+        _ => {
+            let mut count = 0;
+            //Up
+            if let Some(new_row) = row.checked_sub(1) {
+                if data[new_row][col] == data[row][col] - 1 {
+                    count = count + calc_graded_trailheads(data, (new_row, col));
+                }
+            }
+            //Down
+            if row + 1 < data.len() {
+                if data[row + 1][col] == data[row][col] - 1 {
+                    count = count + calc_graded_trailheads(data, (row + 1, col));
+                }
+            }
+            //Left
+            if let Some(new_col) = col.checked_sub(1) {
+                if data[row][new_col] == data[row][col] - 1 {
+                    count = count + calc_graded_trailheads(data, (row, new_col));
+                }
+            }
+            //Right
+            if col + 1 < data[0].len() {
+                if data[row][col + 1] == data[row][col] - 1 {
+                    count = count + calc_graded_trailheads(data, (row, col + 1));
+                }
+            }
+            count
+        }
+    }
 }
